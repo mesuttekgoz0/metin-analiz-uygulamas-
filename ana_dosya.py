811faccc90c16eb785c72c7f84686e43dbeca391
Mesut <mesuttekgoz0@gmail.com>
2024-05-23 17:14:39 +0300

    .

--- ana_dosya.py
+++ ana_dosya.py
@@ -20,6 +20,32 @@ def temizle():
             temiz_metin += kelime + " "
     text.delete("1.0", tk.END)
     text.insert(tk.END, temiz_metin)
+def benzerlik_analizi():
+    metin1_icerik = text1.get("1.0", tk.END)
+    metin2_icerik = text2.get("1.0", tk.END)
+    
+    benzerlik_orani = Levenshtein.ratio(metin1_icerik, metin2_icerik)
+    
+    # Benzerlik oranını yeni bir pencerede göster
+    benzerlik_penceresi = tk.Toplevel(pencere)
+    benzerlik_penceresi.title("Benzerlik Analizi")
+    benzerlik_penceresi.geometry("200x100")
+    
+    benzerlik_etiketi = tk.Label(benzerlik_penceresi, text=f"Benzerlik Oranı: {benzerlik_orani:.2f}")
+    benzerlik_etiketi.pack(pady=20)
+
+def bul_ve_isaretle():
+    kelime = kelime_giris.get().strip() # Giriş kutusundaki kelimeyi alır ve başındaki ve sonundaki boşlukları kaldırır
+    metin = text.get("1.0", tk.END)
+    temiz_metin = metin
+    
+    if kelime and kelime in metin:
+        kelime_baslangic = metin.find(kelime)
+        kelime_bitis = kelime_baslangic + len(kelime)
+        temiz_metin = temiz_metin[:kelime_baslangic] + f"[{kelime}]" + temiz_metin[kelime_bitis:]
+    
+    text.delete("1.0", tk.END)
+    text.insert(tk.END, temiz_metin)
 
 pencere = tk.Tk()
 pencere.title("Python Pencere")
@@ -40,6 +66,11 @@ doysa_yükle_butonu.bind("<Button-1>", dosya_sec)
 temizle_buton = tk.Button(sekme1, text="Temizle", command=temizle)
 temizle_buton.pack()
 
+kelime_giris= tk.Entry(sekme1)
+kelime_giris.pack()
+
+bul_isaretley_butonu = tk.Button(sekme1, text="Kelime Bul ve İşaretle", command=bul_ve_isaretle)
+bul_isaretley_butonu.pack()
 
 sekme2 = ttk.Frame(sekme_kontrol)
 sekme_kontrol.add(sekme2, text="Sekme 2")
@@ -50,9 +81,8 @@ text1.pack(fill=tk.BOTH, expand=0, pady=10)
 text2 = tk.Text(sekme2,width=70,height=2)
 text2.pack(fill=tk.BOTH, expand=0, pady=30)
 
-
-sekme3 = ttk.Frame(sekme_kontrol)
-sekme_kontrol.add(sekme3, text="Sekme 3")
+analiz_butonu = tk.Button(sekme2, text="Benzerliği Analiz Et", command=benzerlik_analizi)
+analiz_butonu.pack()
 
 sekme_kontrol.pack(expand=1, fill="both")
 
